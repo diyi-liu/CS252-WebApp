4f3c1cf3e8696b522a512c1df2b13369823852b7
dzhuang725 <dzhuang725@users.noreply.github.com>
2019-04-21 23:17:58 -0400

    html to js

--- register/register.js
+++ register/register.js
@@ -5,7 +5,7 @@ $(function () {
         submitEvent.preventDefault();
         $.post('/php/Register.php', $(this).serialize(), function (data) {
             if (data === 'ok') {
-                window.location.href = '/MainPage/main.html';
+                window.location.href = '/index.html';
             } else {
                 alert(data);
             }
