bc2182a6c0f3ebf34a44ba5cd48051e558645c49
dzhuang725 <dzhuang725@users.noreply.github.com>
2019-04-26 11:21:06 -0400

    Update index.js

--- rating/index.js
+++ rating/index.js
@@ -16,3 +16,20 @@ $(function () {
     let param = findGetParameter('name');
     $('#title').text(param);
 });
+
+$(function () {
+    $('#inputs').submit(function (submitEvent) {
+        submitEvent.preventDefault();
+        $.post('../php/Rating.php', $(this).serialize(), function(data) {
+            if(data === 'add') {
+                alert('Rating added successfully!');
+                window.location.href = '../index.html';
+            } else if(data === 'update'){
+                alert('Rating updated successfully!');
+                window.location.href = '../index.html';
+            } else{
+                alert('Update rating failed');
+            }
+        });
+    });
+});
