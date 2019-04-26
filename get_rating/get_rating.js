31a5e8cc757a46de49f900b7ba4bdf28b8f7f422
diyi-liu <diyi.liu@outlook.com>
2019-04-26 06:31:34 -0400

    Changed rating design

---
+++ get_rating/get_rating.js
@@ -0,0 +1,8 @@
+// $(function() {
+//     $.get('../php/GetRating.php', {
+//         name: 'test2'
+//     }, function (data) {
+//         // alert(data);
+//         $('#rating-1').text(data);
+//     })
+// });
