4602384c4108fa8b29ce3b109f927b693bc02a1a
dzhuang725 <dzhuang725@users.noreply.github.com>
2019-04-26 14:04:09 -0400

    Update Rating.php

--- php/Rating.php
+++ php/Rating.php
@@ -14,8 +14,7 @@ $result = $stmt->get_result();
 if ($result->num_rows !== 0 ) {
 	// Update the rating
 	$stmt->close();
-	$stmt = $db->prepare('UPDATE `events` SET `rating` = ? WHERE `events`.`uid` = ? and `events`.`ename` = ?');
-	session_start();
+	$stmt = $db->prepare("UPDATE `events` SET `rating` = ? WHERE `events`.`uid` = ? and `events`.`ename` = ?");
 	$stmt->bind_param('dis', $_POST['rating'], $_SESSION['id'], $_POST['ename']);
 	$stmt->execute();
 	$stmt->close();
@@ -23,7 +22,6 @@ if ($result->num_rows !== 0 ) {
 } else {
 	$stmt->close();
 	$stmt = $db->prepare("INSERT INTO `events` (`e_id`, `ename`, `rating`, `uid`) VALUES (NULL, ?, ?, ?)");
-	session_start();
 	$stmt->bind_param('sdi', $_POST ['ename'], $_POST ['rating'], $_SESSION['id']);
 	$stmt->execute();
 	$stmt->close();
