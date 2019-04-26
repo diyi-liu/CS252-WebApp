449eef2021b5d0181e2fd876b1ddc1d80c98a916
dzhuang725 <dzhuang725@users.noreply.github.com>
2019-04-26 01:29:35 -0400

    Final fixed on Login.php

--- php/Login.php
+++ php/Login.php
@@ -1,6 +1,6 @@
 <?php
 $db = new mysqli('localhost', 'e-rater', 'e-rater', 'e-rater');
-$stmt = $db->prepare('select uid from users where username = ? and passord = ? limit 1');
+$stmt = $db->prepare('SELECT uid FROM users WHERE username = ? AND password = ? LIMIT 1');
 $stmt->bind_param('ss', $_POST['username'], $_POST['password']);
 $stmt->execute();
 $result = $stmt->get_result();
