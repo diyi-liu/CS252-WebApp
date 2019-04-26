b03d784ec871490978dcaac681f2f1b6008afd74
dzhuang725 <dzhuang725@users.noreply.github.com>
2019-04-26 13:08:12 -0400

    Update Rating.php

--- php/Rating.php
+++ php/Rating.php
@@ -3,7 +3,7 @@ $db = new mysqli('e-rater.diyi-liu.com', 'e-rater', 'e-rater', 'e-rater');
 // check if this user has a rating on the event already
 $stmt = $db->prepare('SELECT uid FROM events WHERE uid = ? AND ename = ?');
 session_start();
-$stmt->bind_param('ss', $_SESSION['id'], $_POST['ename']);
+$stmt->bind_param('is', $_SESSION['id'], $_POST['ename']);
 $stmt->execute();
 $result = $stmt->get_result();
 if ($result->num_rows !== 0 ) {
@@ -11,14 +11,14 @@ if ($result->num_rows !== 0 ) {
 	$stmt->close();
 	$stmt = $db->prepare('UPDATE `events` SET `rating` = ? WHERE `events`.`uid` = ? and `events`.`ename` = ?');
 	session_start();
-	$stmt->bind_param('ss', $_POST['rating'], $_SESSION['id'], $_POST['ename']);
+	$stmt->bind_param('iis', $_POST['rating'], $_SESSION['id'], $_POST['ename']);
 	$stmt->execute();
 	$stmt->close();
 	echo 'update';
 } else {
 	$stmt->close();
 	$stmt = $db->prepare("INSERT INTO `events` (`e_id`, `ename`, `rating`, `uid`, `s_id`) VALUES (NULL, ?, ?, ?, NULL)");
-	$stmt->bind_param('sss', $_POST ['ename'], $_POST ['rating'], $_SESSION['id']);
+	$stmt->bind_param('ssi', $_POST ['ename'], $_POST ['rating'], $_SESSION['id']);
 	$stmt->execute();
 	$stmt->close();
 	echo 'add';
